444692587fc820027242eaf9f26e6ed5c5fe60b9
Fsher07 <furkansalimm@gmail.com>
2022-09-26 13:54:32 +0300

    Make responsive desing of event page

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,42 +1,58 @@
 import React, { useContext } from "react";
 import { Context } from "../Context";
-import { MdWavingHand } from "react-icons/md";
-import { AiFillCalendar } from "react-icons/ai";
+import { IoIosArrowForward } from "react-icons/io";
+import Calendar from "../assets/Calendar.png";
+import Location from "../assets/Location.png";
 import "./EventPage.css";
 
 function EventPage() {
   const { entries } = useContext(Context);
+
+  const capitalizeFirst = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
   return (
-    <>
-      <div className='event-container'>
-        <img className='eventImage' src={entries.eventImage} alt='event' />
-        <h1>{entries.eventName}</h1>
+    <div className='event-container'>
+      <img className='eventImage' src={entries.eventImage} alt='event' />
+      <div className='event-column-2'>
+        <h1 className='eventTitle title-color'>
+          {capitalizeFirst(entries.eventName)}
+        </h1>
         <h2 className='hostName'>
           Hosted by
-          <span>{entries.hostName}</span>
+          <strong>{entries.hostName}</strong>
         </h2>
         <div className='dateCard'>
-          <AiFillCalendar />
-          <div className='dateCard-container'>
-            <div className='dateCard-context'>
-              <h3>{entries.startDate}</h3>
-              <h3>{entries.startTime}</h3>
+          <div className='dateCard-date'>
+            <div className='icon-container'>
+              <img className='calendar-icon' src={Calendar} alt='calendar' />
             </div>
-            <div className='dateCard-context'>
-              to
-              <strong>
-                <p>
-                  {entries.endDate}
-                  {entries.endTime}
-                </p>
-              </strong>
+            <div className='dateCard-date-container'>
+              <div className='dateCard-context'>
+                <h3 className='context-title title-color'>
+                  {entries.startDate}
+                </h3>
+              </div>
+              <p className='dateCard-context description'>
+                to<strong>&nbsp;{entries.endDate.slice(0, -10)}&nbsp;</strong>
+                {entries.endDate.slice(-10, -3)}
+              </p>
             </div>
+            <IoIosArrowForward color='gray' className='arrow-icon' />
+          </div>
+          <div className='dateCard-address'>
+            <div className='icon-container'>
+              <img src={Location} alt='' />
+            </div>
+            <div>
+              <h3 className='title-color'>Street Name</h3>
+              <p className='description'>{entries.location}</p>
+            </div>
+            <IoIosArrowForward color='gray' className='arrow' />
           </div>
         </div>
-        <h3>Street Name</h3>
-        <h3>{entries.location}</h3>
       </div>
-    </>
+    </div>
   );
 }
 
